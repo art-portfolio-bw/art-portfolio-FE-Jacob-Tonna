5b340ec46fa65ace8707a82b54837988fb626da8
Jtonna <34466548+Jtonna@users.noreply.github.com>
2019-05-23 16:41:04 -0700

    Continuing setup of reducer
    Continued setup of reducer

--- fotograph-app/src/reducers/index.jsx
+++ fotograph-app/src/reducers/index.jsx
@@ -6,3 +6,23 @@ import {
 	REGISTER_SUCCESS,
 	REGISTER_FAILURE,
 } from '../actions'
+
+const initialState = {
+	//LogIn Stuff
+	LoggingIn: false,
+	loggedIn: false,
+	loginError: '',
+
+	// SignUp Stuff
+	Registering: false,
+	Registered: false,
+	RegistrationError: '',
+
+	// stretch stuff
+};
+
+// reducer
+
+const reducer = (state = initialState, action) =>{
+	
+}
