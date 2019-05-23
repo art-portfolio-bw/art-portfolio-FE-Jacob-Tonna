75b8a9949d823fda35ba63be9a43447ec85e1b8a
Jtonna <34466548+Jtonna@users.noreply.github.com>
2019-05-23 11:45:15 -0700

    finished action types + added a personal stretch goal

--- fotograph-app/src/actions/index.jsx
+++ fotograph-app/src/actions/index.jsx
@@ -1,6 +1,9 @@
 import axios from 'axios';
 import { token } from '../token';
 
+const URL = 'https://artportfoliobw.herokuapp.com';
+// .post(`${URL}/SomeLinkHere`, somethingProbablyCreds)
+
 // logging in
 
 export const LOGIN_START = 'LOGIN_START';
@@ -12,7 +15,7 @@ export const login = creds => dispatch => {
 		type: LOGIN_START,
 	});
 	return axios
-		.post('https://artportfoliobw.herokuapp.com/login', creds)
+		.post(`${URL}/login`, creds)
 		.then(res =>{
 			console.log('actions index login:', res)
 			localStorage.setItem('token', res.data.token);
@@ -36,6 +39,22 @@ export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
 export const REGISTER_FAILURE = 'LOGIN_FAILURE';
 
 export const register = creds => dispatch => {
-	
+	dispatch({
+		type: REGISTER_START
+	});
+	return axios
+		.post(`${URL}/signup`, creds)
+		.then(res => {
+			localStorage.setItem('token', res.data.token);
+			dispatch({
+				type: REGISTER_SUCCESS,
+				payload: res.data,
+			})
+		});
 }
 
+// personal stretch 
+
+export const GET_USERS_POSTS_START = 'GET_USERS_POSTS_START';
+export const GET_USERS_POSTS_SUCCESS = 'GET_USERS_POSTS_SUCCESS';
+export const GET_USERS_POSTS_FAILURE = 'GET_USERS_POSTS_FAILURE';
