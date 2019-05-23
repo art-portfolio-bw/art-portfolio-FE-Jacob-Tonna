ba245dfd19916b53761d3c509cf5653b5a081c70
Jtonna <34466548+Jtonna@users.noreply.github.com>
2019-05-22 19:35:44 -0700

    fixed navbar routing, added components
    fixed navbar routing and added popular and recent components

--- fotograph-app/src/components/nav/NavagationBar.jsx
+++ fotograph-app/src/components/nav/NavagationBar.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 function NavagationBar (){
 	return(
 		<div className="navagationbar-container">
-		<Link to='/LandingPage' className='navlink'> Logo </Link>{' '}
+		<Link to='/' className='navlink'> Logo </Link>{' '}
 		<Link to='/SignUp' className='navlink'> sign up </Link> {' '}
 		<Link to='/LogIn' className='navlink'> log in </Link> {' '}
 		</div>
