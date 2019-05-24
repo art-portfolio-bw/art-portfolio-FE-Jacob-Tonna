7b12a5b07b0663d761ca38d13331784e5f01ed7e
Jtonna <34466548+Jtonna@users.noreply.github.com>
2019-05-23 19:04:30 -0700

    Update NavagationBar.jsx

--- fotograph-app/src/components/nav/NavagationBar.jsx
+++ fotograph-app/src/components/nav/NavagationBar.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { connect } from 'react-router-dom';
+import {withRouter} from 'react-router-dom';
+import { Link, NavLink} from 'react-router-dom';
+import {logout} from '../../actions/index';
 
 import logo from '../../assets/LOGO.svg'
 
-function NavagationBar (){
+function NavagationBar (props){
+	console.log('navbar.jsx says:', props)
+	
 	return(
 		<div className="navagationbar-container">
 		<Link to='/' className='navlink'> <img src={logo} alt="fotograph company logo"/> </Link>{' '}
